0b09ad5592ffbe1e5e28442969134ab6e14da620
carloscarl <olcsak@gmx.com>
2018-11-08 20:04:24 +0100

    Google MAP fragment added

--- app/src/main/java/hu/uniobuda/nik/fitorfull/MainActivity.kt
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/MainActivity.kt
@@ -2,12 +2,24 @@ package hu.uniobuda.nik.fitorfull
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //TODO: make a new map
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+
+        })
     }
 }
