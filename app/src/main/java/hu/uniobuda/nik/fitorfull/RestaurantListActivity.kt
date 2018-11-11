ad8fd05e5b6b7d64fc83ce92ab514757c07662f6
Dani János <dani.janos@gmail.com>
2018-11-11 23:47:14 +0100

    Add view, viewAdapter and viewLayoutManager private fields to RestaurantListActivity [issue #7]

--- app/src/main/java/hu/uniobuda/nik/fitorfull/RestaurantListActivity.kt
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/RestaurantListActivity.kt
@@ -2,8 +2,13 @@ package hu.uniobuda.nik.fitorfull
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 
 class RestaurantListActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
