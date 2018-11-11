1d96a5af4b6fb33bb42409a8c916f7c0a08918b9
Dani János <dani.janos@gmail.com>
2018-11-12 00:17:28 +0100

    Add RestaurantViewHolder class to RestaurantListAdapter [issue #7]

--- app/src/main/java/hu/uniobuda/nik/fitorfull/Adapters/RestaurantListAdapter.kt
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/Adapters/RestaurantListAdapter.kt
@@ -1,10 +1,14 @@
 package hu.uniobuda.nik.fitorfull.Adapters
 
 import android.support.v7.widget.RecyclerView
+import android.widget.TextView
 import hu.uniobuda.nik.fitorfull.Models.Restaurant
 
 class RestaurantListAdapter(
         private val restaurantList: Array<Restaurant>
 ) : RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder>() {
+
+    class RestaurantViewHolder(val restaurantView: TextView)
+        : RecyclerView.ViewHolder(restaurantView)
 
 }
