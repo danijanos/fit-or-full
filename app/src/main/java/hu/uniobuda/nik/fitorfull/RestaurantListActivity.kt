14b5a41c734e8ffd9569575b0eb430e8501d5ff9
Dani János <dani.janos@gmail.com>
2018-11-11 19:55:20 +0100

    Add new RestaurantListActivity to the application [issue #7]

---
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/RestaurantListActivity.kt
@@ -0,0 +1,12 @@
+package hu.uniobuda.nik.fitorfull
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RestaurantListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_list)
+    }
+}
