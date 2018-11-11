0da5cc337964add14313b7b15107863c64e40ac0
Dani János <dani.janos@gmail.com>
2018-11-11 23:38:44 +0100

    Add and implement Parcelable interface to the restaurant model [issue #7]

--- app/src/main/java/hu/uniobuda/nik/fitorfull/Models/Restaurant.kt
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/Models/Restaurant.kt
@@ -1,10 +1,39 @@
 package hu.uniobuda.nik.fitorfull.Models
 
-import java.util.*
+import android.os.Parcel
+import android.os.Parcelable
 
 data class Restaurant(
         var name: String,
         var address: String,
-        var openingHours: Date,
+        var openingHours: String,
         var phoneNumber: String
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(address)
+        parcel.writeString(openingHours)
+        parcel.writeString(phoneNumber)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Restaurant> {
+        override fun createFromParcel(parcel: Parcel): Restaurant {
+            return Restaurant(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Restaurant?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
