2a2142f4544d5d92d440cb57f8898e285196405c
Dani János <dani.janos@gmail.com>
2018-11-12 00:19:43 +0100

    Override onCreateViewHolder function [issue #7]

--- app/src/main/java/hu/uniobuda/nik/fitorfull/Adapters/RestaurantListAdapter.kt
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/Adapters/RestaurantListAdapter.kt
@@ -1,8 +1,11 @@
 package hu.uniobuda.nik.fitorfull.Adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.TextView
 import hu.uniobuda.nik.fitorfull.Models.Restaurant
+import hu.uniobuda.nik.fitorfull.R
 
 class RestaurantListAdapter(
         private val restaurantList: Array<Restaurant>
@@ -11,4 +14,13 @@ class RestaurantListAdapter(
     class RestaurantViewHolder(val restaurantView: TextView)
         : RecyclerView.ViewHolder(restaurantView)
 
+    override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int): RestaurantListAdapter.RestaurantViewHolder {
+
+        val restaurantView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.restaurant_view, parent, false) as TextView
+
+        return RestaurantViewHolder(restaurantView)
+    }
 }
