122c588ff24a03ce22079db7b1b8771506b0250d
Dani János <dani.janos@gmail.com>
2018-11-12 00:14:54 +0100

    Start to implemet RestaurantListAdapter [issue #7]

---
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/Adapters/RestaurantListAdapter.kt
@@ -0,0 +1,10 @@
+package hu.uniobuda.nik.fitorfull.Adapters
+
+import android.support.v7.widget.RecyclerView
+import hu.uniobuda.nik.fitorfull.Models.Restaurant
+
+class RestaurantListAdapter(
+        private val restaurantList: Array<Restaurant>
+) : RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder>() {
+
+}
