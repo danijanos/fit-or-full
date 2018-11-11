b2c34b4a50e902f2c1a5861a1748975d1e3d1b9e
Dani János <dani.janos@gmail.com>
2018-11-12 00:42:59 +0100

    Add an array of restaurants as a private field to RestaurantListActivity [issue #7]

--- app/src/main/java/hu/uniobuda/nik/fitorfull/RestaurantListActivity.kt
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/RestaurantListActivity.kt
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import hu.uniobuda.nik.fitorfull.adapters.RestaurantListAdapter
 import hu.uniobuda.nik.fitorfull.models.Restaurant
+import java.util.jar.Attributes
 
 class RestaurantListActivity : AppCompatActivity() {
+
+    private lateinit var restaurants: Array<Restaurant>
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
@@ -17,6 +21,6 @@ class RestaurantListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_restaurant_list)
 
         viewManager = LinearLayoutManager(this)
-        viewAdapter = RestaurantListAdapter(Restaurant)
+        viewAdapter = RestaurantListAdapter(restaurants)
     }
 }
