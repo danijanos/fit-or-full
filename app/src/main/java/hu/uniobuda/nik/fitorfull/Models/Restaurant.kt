98e98191e5e5511703d77731c5990dd93ada2d6d
Dani János <dani.janos@gmail.com>
2018-11-11 23:33:29 +0100

    Add a restaurant model POCO [issue #7]

---
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/Models/Restaurant.kt
@@ -0,0 +1,10 @@
+package hu.uniobuda.nik.fitorfull.Models
+
+import java.util.*
+
+data class Restaurant(
+        var name: String,
+        var address: String,
+        var openingHours: Date,
+        var phoneNumber: String
+)
