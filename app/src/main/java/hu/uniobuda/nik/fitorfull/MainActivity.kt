c8167073f0bde828b805029a8da20d414089fa5b
carloscarl <olcsak@gmx.com>
2018-12-03 19:49:55 +0100

    Issue 6: barnch created, restaurant file created

--- app/src/main/java/hu/uniobuda/nik/fitorfull/MainActivity.kt
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/MainActivity.kt
@@ -1,7 +1,9 @@
 package hu.uniobuda.nik.fitorfull
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -11,6 +13,25 @@ class MainActivity : AppCompatActivity() {
     lateinit var mapFragment: SupportMapFragment
     lateinit var googleMap: GoogleMap
 
+    fun createLocalDatabase() {
+        val FILENAME = "restaurants.dat"
+        val saveThis = "Helló Storage!"
+        val fos = openFileOutput(FILENAME, Context.MODE_PRIVATE)
+        fos.write(saveThis.toByteArray())
+        fos.close()
+    }
+
+    fun loadRestaurantData(){
+        val fis = openFileInput("restaurants.dat")
+        val buffer = ByteArray(1024)
+        var len: Int
+        len = fis.read(buffer)
+        while (len > 0) {
+            Log.d("TAG", String(buffer, 0, len))
+            len = fis.read(buffer)
+        }
+        fis.close()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +42,10 @@ class MainActivity : AppCompatActivity() {
             googleMap = it
 
         })
+
+        createLocalDatabase()
+
+        loadRestaurantData()
+
     }
 }
