d968dfb4d7af3849ac446217e8f6bed5c3cb7b06
carloscarl <olcsak@gmx.com>
2018-12-04 08:35:13 +0100

    Issue 6: first marker created, map zoom applied

--- app/src/main/java/hu/uniobuda/nik/fitorfull/MainActivity.kt
+++ app/src/main/java/hu/uniobuda/nik/fitorfull/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,15 +40,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(OnMapReadyCallback {
-            googleMap = it
-
-        })
-
         createLocalDatabase()
 
         loadRestaurantData()
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+            googleMap.isMyLocationEnabled = true
+
+            val location1 = LatLng(47.53505161,19.03607726)
+            googleMap.addMarker(MarkerOptions().position(location1).title("My Location"))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,15f))
+
+        })
+
+
 
     }
 }
